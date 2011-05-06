82419e3e1341189186d269be80c191d705fc4508
Jian Ma <jianmov@gmail.com>
2011-05-05 23:44:22 -0700

    CodeOfShame

--- CheckerBoard.py
+++ CheckerBoard.py
@@ -49,26 +49,26 @@ class CheckerBoard:
 	def checkMove(self, start, end, game_state):
 		if start.row%2 + start.column%2 != 1 or end.row%2 + end.column%2 !=1:
 			return False
-		if not (self.board[start.row][start.column]+1)//2 == game_state.get_state() or not self.board[end.row][end.column] == SquareState.EMPTY:
+		if not (self.board[start.column][start.row]+1)//2 == game_state.get_state() or not self.board[end.column][end.row] == SquareState.EMPTY:
 			return False
-		if not end.column - start.column == abs(end.column - start.column)*(game_state.get_state()*2-3) and not self.board[start.row][start.column] == SquareState.WHITEKING and not self.board[start.row][start.column] == SquareState.BLACKKING:
+		if not end.row - start.row == abs(end.row - start.row)*(game_state.get_state()*2-3) and not self.board[start.column][start.row] == SquareState.WHITEKING and not self.board[start.column][start.row] == SquareState.BLACKKING:
 			return False
 		if abs(end.column - start.column) == 1 and abs(end.row - start.row) == 1 and not self.anyJump(game_state):
 			return True
-		return abs(end.column - start.column) == 2 and abs(end.row - start.row) == 2 and self.board[(start.row + end.row)/2][(start.column + end.column)/2] == 5 - game_state.get_state()*2
+		return abs(end.column - start.column) == 2 and abs(end.row - start.row) == 2 and (self.board[(start.column + end.column)/2][(start.row + end.row)/2] == 5 - game_state.get_state()*2 or self.board[(start.column + end.column)/2][(start.row + end.row)/2] == 6 - game_state.get_state()*2)
 
 
 	# Makes the given move.  Returns true if the player has another move, else false
 	# Inputs are two point objects: the start point and the end point
 	def move(self, start, end, game_state):
-		self.board[end.row][end.column] = self.board[start.row][start.column]
-		self.board[start.row][start.column] = SquareState.EMPTY
+		self.board[end.column][end.row] = self.board[start.column][start.row]
+		self.board[start.column][start.row] = SquareState.EMPTY
 		if abs(start.row - end.row) == 2:
-			self.board[(start.row+end.row)/2][(start.column+end.column)/2] = SquareState.EMPTY
-		if end.column == 7 and game_state.get_state() == 2:
-			self.board[end.row][end.column] = SquareState.BLACKKING
-		elif end.column == 0 and game_state.get_state() == 1:
-			self.board[end.row][end.column] = SquareState.WHITEKING
+			self.board[(start.column+end.column)/2][(start.row+end.row)/2] = SquareState.EMPTY
+		if end.row == 7 and game_state.get_state() == 2:
+			self.board[end.column][end.row] = SquareState.BLACKKING
+		elif end.row == 0 and game_state.get_state() == 1:
+			self.board[end.column][end.row] = SquareState.WHITEKING
 		step = 0
 		#TODO: Shouldn't check for jumps unless the move just made was a jump
 		if self.anyJump(game_state):
@@ -80,8 +80,8 @@ class CheckerBoard:
 	def getMoves(self,start,game_state):
 		possibleMoves = []
 		for step in range(8):
-			x = int(start.row + (step//4+1)*cos(pi/4+pi/2*step)/abs(cos(pi/4+pi/2*step)))
-			y = int(start.column + (step//4+1)*sin(pi/4+pi/2*step)/abs(sin(pi/4+pi/2*step)))
+			x = int(start.column + (step//4+1)*cos(pi/4+pi/2*step)/abs(cos(pi/4+pi/2*step)))
+			y = int(start.row + (step//4+1)*sin(pi/4+pi/2*step)/abs(sin(pi/4+pi/2*step)))
 			if x <= 7 and x >= 0 and y <= 7 and y >= 0:
 				end = Point(x,y)
 				if self.checkMove(start,end,game_state):
@@ -92,9 +92,9 @@ class CheckerBoard:
 	# Returns a list of all possible moves this player can make
 	def getAllMoves(self, game_state):
 		moves = []
-		for row in range(len(self.board)):
-			for col in range(len(self.board[row])):
-				start = Point(row, col)
+		for column in range(len(self.board)):
+			for row in range(len(self.board[column])):
+				start = Point(col, row)
 				availbleMoves = self.getMoves(start, game_state)
 				if not not availbleMoves:
 					for end in availbleMoves:
@@ -104,9 +104,9 @@ class CheckerBoard:
 	# private function 
 	# Return ture if there are any pieces that can jump
 	def anyJump(self, game_state):
-		for row in range(len(self.board)):
-			for col in range(len(self.board[row])):
-				start = Point(row, col)
+		for col in range(len(self.board)):
+			for row in range(len(self.board[col])):
+				start = Point(col, row)
 				if self.canJumps(start, 0, game_state):
 					return True
 		return False
@@ -116,13 +116,13 @@ class CheckerBoard:
 	def canJumps(self, start, step, game_state):
 		if step == 4:
 			return False
-		if not (self.board[start.row][start.column]+1)//2 == game_state.get_state():
+		if not (self.board[start.column][start.row]+1)//2 == game_state.get_state():
 			return False
-		x = int(start.row + 2*cos(pi/4+pi/2*step)/abs(cos(pi/4+pi/2*step)))
-		y = int(start.column + 2*sin(pi/4+pi/2*step)/abs(sin(pi/4+pi/2*step)))
+		x = int(start.column + 2*cos(pi/4+pi/2*step)/abs(cos(pi/4+pi/2*step)))
+		y = int(start.row + 2*sin(pi/4+pi/2*step)/abs(sin(pi/4+pi/2*step)))
 		if x <= 7 and x >= 0 and y <= 7 and y >= 0:
-			if y - start.column == abs(y - start.column)*(game_state.get_state()*2-3) or self.board[start.row][start.column] == SquareState.WHITEKING or self.board[start.row][start.column] == SquareState.BLACKKING:
-				if self.board[(start.row + x)/2][(start.column + y)/2] == 5 - game_state.get_state()*2:
-					if self.board[x][y] == SquareState.EMPTY and self.board[start.row][start.column] != SquareState.EMPTY:
+			if y - start.row == abs(y - start.row)*(game_state.get_state()*2-3) or self.board[start.column][start.row] == SquareState.WHITEKING or self.board[start.column][start.row] == SquareState.BLACKKING:
+				if self.board[(start.column + x)/2][(start.row + y)/2] == 5 - game_state.get_state()*2 or self.board[(start.column + x)/2][(start.row + x)/2] == 6 - game_state.get_state()*2:
+					if self.board[x][y] == SquareState.EMPTY and self.board[start.column][start.row] != SquareState.EMPTY:
 						return True
 		return True and self.canJumps(start, step+1,game_state)
